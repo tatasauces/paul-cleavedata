aad3079663ee9a2f0e81674b4d38f10db5f34e30
tatasauce0911 <tatasauce0911@gmail.com>
2026-02-10 07:34:08 +0000

    人工檢視抽樣

--- alignment_cleaning/human/human.py
+++ alignment_cleaning/human/human.py
@@ -4,6 +4,6 @@ import pandas as pd
 path = "/home/user/paul-cleavedata/alignment_cleaning/qwen/alignment_scores_full.csv"
 
 df = pd.read_csv(path)
-df = df.loc[df['comet_score'] <= 0.75,["src","mt"]]
-df = df.sample(n=100)
-print(df)
+df = df.loc[df['comet_score'] <= 0.75,["src","mt","comet_score","source_file","line_idx"]]
+df = df.sample(n=100,random_state=42)
+df.to_csv("random_sample.csv", index=False, encoding='utf-8-sig')
